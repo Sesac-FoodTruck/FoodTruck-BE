f4a26746308a005bfaa47a734c6ed51fe153877d
hjcho <hjcho.sesac@gmail.com>
2024-02-04 20:11:01 +0900

    cors fix

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/public', express.static(path.join(__dirname, './public')));
 // cors : 보안수준 낮게
 app.use(cors({
     // origin: "http://localhost:3000",
-    origin: ['https://www.yummytruck.shop, http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000', 'http://aws.amazon.com'],
+    origin: ['https://www.yummytruck.shop', 'http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000', 'http://aws.amazon.com'],
     credentials: true,
 }));
 
