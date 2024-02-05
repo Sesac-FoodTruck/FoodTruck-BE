7af5a1d4d00a7e1d734c5c71992eed0f29ce19db
sangyeon Lee <asylee02@gmail.com>
2024-02-05 14:28:02 +0900

    카카오 로그인 수정

--- src/strategies/kakao.js
+++ src/strategies/kakao.js
@@ -1,12 +1,17 @@
 const passport = require('passport');
 const KakaoStrategy = require('passport-kakao').Strategy;
 const axios = require('axios');
-require('dotenv').config();
-const API_BASE_URL = 'http://localhost:4000';
+const cookieParser = require('cookie-parser');
+const express = require('express');
 
+const app = express();
+require('dotenv').config();
 
 // DB 직접 접속
+
 const mysql = require('mysql2/promise'); // mysql2/promise 모듈 추가
+
+app.use(cookieParser());
 
 const dbConfig = {
     host: "www.yummytruck.store",
@@ -21,64 +26,71 @@ const dbConfig = {
 
 passport.use(new KakaoStrategy({
     clientID: process.env.KAKAO_CLIENT_ID,
-    callbackURL: process.env.KAKAO_CALLBACK_URL,
-    callbackURL: '/auth/kakao/callback'
+    callbackURL: 'http://www.yummytruck.store/auth/kakao/callback'
+    // callbackURL: 'http://localhost:4000/auth/kakao/callback'
 }, kakaoStrategyCallback));
 
 async function kakaoStrategyCallback(accessToken, refreshToken, profile, done) {
     let dbConnection = null;
 
     try {
-        dbConnection = await mysql.createConnection(dbConfig); // 데이터베이스 연결
+        const dbConnection = await mysql.createConnection(dbConfig); // 데이터베이스 연결
 
         const [existingUser] = await dbConnection.query('SELECT * FROM member WHERE social_id = ?', [profile.id]);
         if (existingUser.length > 0) {
             const user = existingUser[0];
-            done(null, user); // 기존 사용자인 경우 로그인 처리 후 리다이렉트
+            return done(null, user);
         } else {
             // 새로운 사용자 등록
-            const response = await axios.post(`${API_BASE_URL}/memberRegister`, {
+            const newUser = await axios.post('/memberRegister', {
                 nickname: profile.username,
                 social_id: profile.id,
                 social_code: 1, // 카카오 코드
                 social_token: accessToken
             });
-
-            if (response.status === 201) { // 회원가입 성공
-                const newUser = response.data;
-                done(null, newUser); // 회원가입 후 로그인 처리
-            } else {
-                throw new Error('회원가입 실패');
-            }
+            return done(null, newUser.data);
         }
     } catch (error) {
-        console.error('회원 등록 중 에러 발생:', error);
         done(error);
     } finally {
-        if (dbConnection) {
-            await dbConnection.end();
+        if (dbConnection && dbConnection.end) {
+            await dbConnection.end(); // 데이터베이스 연결 종료
         }
     }
 }
 
-passport.serializeUser((user, done) => {
-    done(null, user);
+passport.serializeUser((user,done)=>{
+    return done(null, user);
 });
 
-passport.deserializeUser((user, done) => {
+passport.deserializeUser((user, done)=>{
     console.log('deserializeUser user : ', users)
-    done(null, user);
+    return done(null,user);
 })
+
+// module.exports = (app) => {
+//     app.get('/auth/kakao', passport.authenticate('kakao'));
+//     app.get('/auth/kakao/callback', passport.authenticate('kakao', {
+//         failureRedirect: '/', // 로그인 실패시 리다이렉트
+//     }), (req, res) => {
+//         console.log('전달 ID: ',req.user.id);
+//         res.redirect(`/member?id=${req.user.id}`); // 성공 시 대시보드로 리다이렉트
+//     });
+// };
 
 module.exports = (app) => {
     app.get('/auth/kakao', passport.authenticate('kakao'));
+
     app.get('/auth/kakao/callback', passport.authenticate('kakao', {
         failureRedirect: '/',
     }), (req, res) => {
         res.cookie('userId', req.user.id, { httpOnly: true });
-
-        const redirectUrl = req.cookies.originalUrl || '/';
-        console.log('redirectUrl : ', redirectUrl)
+        // res.cookie('test', 'hello~');
+        console.log(req.user.id)
+        // console.log(req)
+        // console.log(res)
+        const redirectUrl = 'https://www.yummytruck.shop/mypage';
+        // const redirectUrl = 'http://localhost:3000/mypage';
         res.redirect(redirectUrl);
     });
 };
